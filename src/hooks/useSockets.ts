dbfb8dbeb694ee96d9313ed8f198620bf1da9420
feredev <feredev2@gmail.com>
2024-12-15 16:54:52 -0400

    chore: add a unuseful backend_url constant

--- src/hooks/useSockets.ts
+++ src/hooks/useSockets.ts
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import io, { Socket } from "socket.io-client";
 
+const IS_PROD = false;
+const BACKEND_URL = IS_PROD ? 'http://localhost:4050' : 'http://localhost:4050';
+
 const useSocket = (): Socket | null => {
     const [socket, setSocket] = useState<Socket | null>(null);
     
     useEffect(() => {
-        const socket = io('http://localhost:4050');
+        const socket = io(BACKEND_URL);
         setSocket(socket);
 
         return () => {
